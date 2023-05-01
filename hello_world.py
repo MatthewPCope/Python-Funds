483688d15a99386c153c7bd7442fd478d6f5f821
MatthewPCope <126289016+MatthewPCope@users.noreply.github.com>
2023-05-01 13:16:40 -0700

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,26 @@
+# 1
+print("Hello World")
+
+# 2a
+name = "Noelle"
+print("Hello", name,"!")
+# 2b
+print("Hello " + name + "!")
+
+#3a
+name = 42
+print("Hello", name,"!")
+# b 
+print("Hello "+ str(name)+"!")
+
+# 4a
+fave_food1 = "sushi"
+fave_food2= "pizza"
+print("I love to eat {} and {}".format(fave_food1, fave_food2))
+# 4b
+print(f"I love to eat {fave_food1} and {fave_food2}")
+
+total = 35
+user_val = "26"
+total = total + int(user_val)
+print(total)
